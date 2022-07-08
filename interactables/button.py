d6581d2759921835733f9ca1b6cfc34830e2e170
Marcelo Vogt <marcelo.vogt@explot.com>
2022-07-07 22:18:00 -0300

    Adicionada fase 6 e corrigido bug no botão

--- interactables/button.py
+++ interactables/button.py
@@ -1,6 +1,5 @@
 from interactable import Interactable
-from objects.weight import Weight
-from objects.player import Player
+from objects.moveable import Moveable
 from event import Event
 from pygame import mixer
 
@@ -8,12 +7,12 @@ class Button(Interactable):
     def __init__(self, x, y, tileset):
         super().__init__(x, y, tileset, 262)
     def interact(self, obj):
-        if type(obj) is Weight or type(obj) is Player:
+        if issubclass(type(obj), Moveable):
             button_sound = mixer.Sound("sounds_effects/button_press.mp3")
             button_sound.set_volume(0.5)
             button_sound.play()
             return Event.BUTTON_PRESS
 
     def uninteract(self, obj):
-        if type(obj) is Weight or type(obj) is Player:
+        if issubclass(type(obj), Moveable):
             return Event.BUTTON_UNPRESS
