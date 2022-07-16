9bc0af80cee59f79f2f415c2d7097eb10a4b5d79
Marcelo Vogt <marcelo.vogt@explot.com>
2022-07-16 17:55:02 -0300

    Fixed Hole

--- interactables/hole.py
+++ interactables/hole.py
@@ -1,6 +1,9 @@
 from interactable import Interactable
 from event import Event
 from objects.moveable import Moveable
+from objects.player import Player
+from objects.weight import Weight
+
 
 class Hole(Interactable):
     def __init__(self, x, y, tileset):
@@ -10,3 +13,7 @@ class Hole(Interactable):
         if issubclass(type(obj), Moveable):
             obj.die()
             obj.disappear()
+            if type(obj) is Player:
+                return Event.PLAYER_DIE
+            elif type(obj) is Weight:
+                return Event.WEIGHT_DIE
